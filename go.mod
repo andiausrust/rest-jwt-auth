4ae542d835b7f41d98fba8b48a98eed2d1f2ed68
Andi <familie.mayer@aon.at>
2019-06-05 10:04:28 +0200

    implemented hash2

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/andiausrust/rest-jwt-auth
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.2
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.7.2
+	github.com/lib/pq v1.1.1
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
+)
--- main.go
+++ main.go
@@ -1,21 +1,125 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
 
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+var db *sql.DB
 
 func main() {
+
+	pgUrl, err := pq.ParseURL("postgres://ekfdgdov:y2jxhB-3xJAk_K6G-75KU0fqAl-pbTGT@manny.db.elephantsql.com:5432/ekfdgdov")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err = sql.Open("postgres", pgUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/protected", TokenVerifyMiddleware(ProtectedEndpoint)).Methods("GET")
+	router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
 
+	log.Println("Listen on server :8888")
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
 
-func signup(w http.ResponseWriter, r *http.Request){}
+func respondWithError(w http.ResponseWriter, status int, error Error) {
+	w.WriteHeader(http.StatusBadRequest)
+	json.NewEncoder(w).Encode(error)
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup invoked")
+	var user User
+	var error Error
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		//respond with error, send status bad request
+		error.Message = "Email is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+	if user.Password == "" {
+		//respond with error, send status bad request
+		error.Message = "Password is missing"
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spew.Dump(user)
+
+	fmt.Println("password txt", user.Password)
+	fmt.Println("hash", hash)
+
+	user.Password = string(hash)
+	fmt.Println("pwd hashed string", user.Password)
+
+	stmt := "insert into users (email, password) values($1, $2) RETURNING id;"
+
+	err = db.QueryRow(stmt, user.Email, user.Password).Scan(&user.ID)
+	if err != nil {
+		error.Message = "Server error"
+		respondWithError(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	user.Password = ""
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, user)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(("login ...."))
+	w.Write([]byte("successfully called login"))
+}
+
+func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("token verify middleware")
+	return nil
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("protectedEndpoint invoked.")
+}
